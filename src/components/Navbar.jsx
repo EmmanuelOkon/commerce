e6d031436264c34bf99bad398e759f9f5061baa2
Emmanuel Okon <emmaudo27@gmail.com>
2021-12-06 00:49:02 +0100

    navbar search bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Search } from '@material-ui/icons';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -8,20 +9,52 @@ const Container = styled.div`
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
+  align-items: center;
   justify-content: space-between;
 `;
 
-const Left = styled.div``
-const Center = styled.div``
-const Right = styled.div``
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`
+
+const Input 
+
+const Center = styled.div`
+  flex: 1;
+`;
+
+const Right = styled.div`
+  flex: 1;
+`;
 
 const Navbar = () => {
   return (
     <Container>
       <Wrapper>
-          <Left>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus magni ratione aliquam facilis omnis temporibus neque repellat ipsa consequuntur, dolore nam at distinctio voluptatum quae ducimus numquam. Porro, quo aut.</Left>
-          <Center>Center</Center>
-          <Right>Right</Right>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+              input
+              <Search />
+          </SearchContainer>
+        </Left>
+        <Center>Center</Center>
+        <Right>Right</Right>
       </Wrapper>
     </Container>
   );
