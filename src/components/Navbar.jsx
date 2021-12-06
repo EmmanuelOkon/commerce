e9075ecd2f51559d9b845f8753073d06e2a43515
Emmanuel Okon <emmaudo27@gmail.com>
2021-12-06 14:42:31 +0100

    desktop navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import { Search } from '@material-ui/icons';
+import { Badge } from "@material-ui/core";
+import { Search, ShoppingCartOutlined } from '@material-ui/icons';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -30,16 +31,33 @@ const SearchContainer = styled.div`
   align-items: center;
   margin-left: 25px;
   padding: 5px;
-`
+`;
 
-const Input 
+const Input = styled.input`
+  border: none;
+  outline: none;
+`;
 
 const Center = styled.div`
   flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
 `;
 
 const Right = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
 `;
 
 const Navbar = () => {
@@ -49,12 +67,22 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-              input
-              <Search />
+            <Input />
+            <Search style={{color: "gray", fontSize: 16}} />
           </SearchContainer>
         </Left>
-        <Center>Center</Center>
-        <Right>Right</Right>
+        <Center>
+          <Logo>LAMA.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color='primary'>
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
